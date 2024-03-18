235b8bdadb1b62246194c8498405c82bd6c5fb59
Sean McAuliffe <sean.mcauliffe@herodigital.com>
2024-03-18 19:10:43 -0400

    testing

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,23 +1,28 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
+  // Change to ul, li
   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
+  [...block.children].forEach((row, index) => {
     const li = document.createElement('li');
-    moveInstrumentation(row, li);
+    li.id = `list-item-${index + 1}`; // Assigning unique IDs to each li
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      if (div.children.length === 1 && div.querySelector('img')) div.className = 'cards-card-image-container';
       else div.className = 'cards-card-body';
     });
     ul.append(li);
   });
   ul.querySelectorAll('img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
+    const container = img.parentElement;
+    container.classList.add('cards-card-image-container');
+    const squareOverlay = document.createElement('div');
+    squareOverlay.classList.add('cards-card-image-overlay');
+    squareOverlay.innerHTML = `<div class="cards-card-image-text">${img.alt}</div>`;
+    container.appendChild(squareOverlay);
+    container.replaceWith(
+      createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]),
+    );
   });
   block.textContent = '';
   block.append(ul);
